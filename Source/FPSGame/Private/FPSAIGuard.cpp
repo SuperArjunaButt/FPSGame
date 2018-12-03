4ef01286a1c42287b683a4bbeda321c0b7884fa2
Arjuna Gonzales <arjuna.gonzales@gmail.com>
2018-12-03 17:58:27 -0500

    Imported dwarf grunt, completed, completed Sec. 4 Lec. 25
    Note: Hat and shoulder pads not texturing right as of this commit.

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -1,12 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSAIGuard.h"
+#include <Perception/PawnSensingComponent.h>
+#include <DrawDebugHelpers.h>
 
 // Sets default values
 AFPSAIGuard::AFPSAIGuard()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	AISensor = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("AI Sensor"));
+
+	AISensor->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnSeePlayer);
+	//AISensor->OnHearNoise.AddDynamic(this, &AFPSAIGuard::OnHear);
 
 }
 
@@ -24,10 +31,19 @@ void AFPSAIGuard::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
-void AFPSAIGuard::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AFPSAIGuard::OnSeePlayer(APawn* SeenPawn)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (!SeenPawn)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("OnSeePlayer called but SeenPawn is null"))
+		return;
+	}
+	
+	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 64.0f, 32, FColor::Red, false, 10.0f);
+}
+
+void AFPSAIGuard::OnHear(APawn* InstigatorPawn, const FVector& Location, float Volume)
+{
 
 }
 
